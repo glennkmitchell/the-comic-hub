cc1b5bfd464a634a9ef7a140c5da955b2d1f811a
Glenn Mitchell <glenn@planetuber.com>
2017-09-08 10:34:54 +1000

    Added TheComicHub.Web and TheComicHub.Web.Tests

---
+++ the-comic-hub/ComicHub.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ComicHub.Web.MvcApplication" Language="C#" %>
---
+++ the-comic-hub/ComicHub.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ComicHub.Web.Startup))]
+namespace ComicHub.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
